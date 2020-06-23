a6d27fe01512ab58934407f71c99aff2296acd0c
pbg0205 <pbg0205@naver.com>
2020-06-23 18:36:36 +0900

    #062320 - codeplus 기초(1/2) - 자료구조(참고)

---
+++ codeplus_algo_basic/src/data_structure_ref/word_length.java
@@ -0,0 +1,22 @@
+package data_structure_ref;
+
+import java.util.Scanner;
+
+public class word_length {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String str = sc.nextLine();
+		int num_of_alpha = 0;
+		
+		for (int i = 0; i < str.length(); i++) {
+			if('a' <= str.charAt(i) && str.charAt(i) <= 'z')	num_of_alpha++;
+			if('A' <= str.charAt(i) && str.charAt(i) <= 'Z')	num_of_alpha++;
+		}
+		
+		System.out.println(num_of_alpha);
+		
+		
+		sc.close();
+	}
+}
