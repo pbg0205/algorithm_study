2f6a4c0ede5e3b2f2472a3ba689fa9098b80cf08
pbg0205 <pbg0205@naver.com>
2020-07-01 23:58:37 +0900

    #070120 codeplus 기초(1/2) 400 - 다이나믹 프로그래밍1 -2xN 타일링2 -1,2,3 더하기

---
+++ codeplus_algo_basic/src/dynamic_programming/n2_tiling_2.java
@@ -0,0 +1,27 @@
+package dynamic_programming;
+
+import java.util.Scanner;
+
+public class n2_tiling_2 {
+	static int[] d = new int[1001];
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);	
+		int n = sc.nextInt();
+		
+		System.out.println(dp(n));
+		
+		sc.close();
+	}
+	
+
+	public static int dp(int n) {
+		if(n == 1)	return d[1] = 1;
+		if(n == 2)	return d[2] = 3;
+		if(d[n] != 0) {
+			return d[n];
+		}else {
+			return d[n] = (2 * dp(n - 2) + dp(n - 1)) % 10007;	
+		}
+	}
+}
