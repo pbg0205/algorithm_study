d7703acffa65cb34215182d37bd13c700a4d2bd9
pbg0205 <pbg0205@naver.com>
2020-07-08 17:26:07 +0900

    #070820 codeplus 다이나믹프로그래밍1 -쉬운 계단수(10884) -이친수(2193) -가장 긴 증가하는 부분 수열(11053)

---
+++ codeplus_algo_basic/src/dynamic_programming/easy_bridge_num.java
@@ -0,0 +1,30 @@
+package dynamic_programming;
+
+import java.util.Scanner;
+
+public class easy_bridge_num {
+	static final int mod = 1000000000;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = Integer.parseInt(sc.nextLine());
+		long[][] stair = new long[n+1][10];
+		
+		for (int i = 1; i <= 9; i++)	stair[1][i] = 1;
+		for (int i = 2; i <= n; i++) {
+			for (int j = 0; j <= 9; j++) {
+				if(j == 0)			stair[i][j] = stair[i-1][j+1];
+				else if(j == 9)		stair[i][j] = stair[i-1][j-1];
+				else				stair[i][j] = stair[i-1][j-1] + stair[i-1][j+1];
+				
+				stair[i][j] %= mod;
+			}
+		}
+		
+		long ans = 0;
+		for (int i = 0; i <= 9; i++)	ans += stair[n][i];
+		System.out.println(ans % mod);
+		
+		sc.close();
+	}
+}
