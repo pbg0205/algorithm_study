cae3de93ecc36abc57924ca503a4cfcb51902192
pbg0205 <pbg0205@naver.com>
2020-07-13 22:30:46 +0900

    #071320 codeplus 다이나믹프로그래밍1 -1. 합분해(2225)

---
+++ codeplus_algo_basic/src/dp1/Dividing_Sum.java
@@ -0,0 +1,35 @@
+package dp1;
+
+import java.util.Scanner;
+
+public class Dividing_Sum {
+	static final long div = 1000000000;
+	static int[][] dp;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+		dp = new int[k+1][n+1];
+		
+		for (int i = 1; i <= n; i++)	dp[1][i] = 1;
+		for (int i = 1; i <= k; i++)	dp[i][0] = 1;
+		
+		for (int row = 1; row <= k; row++) {
+			for (int col = 1; col <= n; col++) {
+				dp[row][col] = (int) ((dp[row-1][col] + dp[row][col-1]) % div);
+			}
+		}
+		
+//		for (int row = 0; row <= k; row++) {
+//			for (int col = 0; col <= n; col++) {
+//				System.out.print(dp[row][col] +  " ");
+//			}
+//			System.out.println();
+//		}
+//		
+		System.out.println(dp[k][n]);
+		
+		sc.close();
+	}
+}
