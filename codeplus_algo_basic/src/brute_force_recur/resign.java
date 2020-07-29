dc1a939bc5d0d6bef1d0ab59795cc80e5a1a8edd
pbg0205 <pbg0205@naver.com>
2020-07-30 01:14:39 +0900

    18일차 - 퇴사(14501)

---
+++ codeplus_algo_basic/src/brute_force_recur/resign.java
@@ -0,0 +1,43 @@
+package brute_force_recur;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class resign {
+
+	static int[] days, cost;
+	static int N, max;
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		N = Integer.parseInt(br.readLine());
+		days = new int[N];
+		cost = new int[N];
+		
+		for (int idx = 0; idx < N; idx++) {
+			st = new StringTokenizer(br.readLine());
+			days[idx] = Integer.parseInt(st.nextToken());
+			cost[idx] = Integer.parseInt(st.nextToken());
+		}
+		
+		schedule(0, 0);
+		System.out.println(max);
+
+		br.close();
+	}
+
+	static void schedule(int day, int money) {
+		if(day>=N) {
+			max = Math.max(max, money);
+			return;
+		}
+		if(day+days[day]<= N)
+				schedule(day+days[day],money+cost[day]);
+			
+		schedule(day+1,money);
+	}
+}
