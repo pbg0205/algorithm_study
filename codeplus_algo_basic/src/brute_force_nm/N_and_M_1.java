fffa82b2040b3ef014f8488c8e4ba6d887985a9e
pbg0205 <pbg0205@naver.com>
2020-08-05 20:02:10 +0900

    15일차 - N과M(1)(15649) : 내용 정리

--- codeplus_algo_basic/src/brute_force_nm/N_and_M_1.java
+++ codeplus_algo_basic/src/brute_force_nm/N_and_M_1.java
@@ -14,11 +14,11 @@ public class N_and_M_1 {
 		data = new int[n];
 		visited = new boolean[n+1];
 		
-		getPair(0);
+		permutation_dfs(0);
 		sc.close();
 	}
 	
-	private static void getPair(int num) {
+	private static void permutation_dfs(int num) {
 		if(num == m) {
 			for (int i = 0; i < m; i++)
 				System.out.print(data[i] + " ");
@@ -26,12 +26,13 @@ public class N_and_M_1 {
 			System.out.println();
 			return ;
 		}
-		
+		//깊은 depth로 들어간 다음, 탈출하는 method - permutation
+		//시작 : 1 → permutation 형태로 접근
 		for (int idx = 1; idx <= n; idx++) {
 			if(!visited[idx]) {
 				visited[idx] = true;
 				data[num] = idx;
-				getPair(num + 1);
+				permutation_dfs(num + 1);
 				visited[idx] = false;
 			}
 		}
