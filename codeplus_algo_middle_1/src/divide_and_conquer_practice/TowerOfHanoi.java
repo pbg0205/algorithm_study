149ff8e8a706c232846ae07383835fa85737c307
pbg0205 <pbg0205@naver.com>
2020-08-19 18:03:49 +0900

    33일차 - 하노이탑 이동순서(11729)

---
+++ codeplus_algo_middle_1/src/divide_and_conquer_practice/TowerOfHanoi.java
@@ -0,0 +1,37 @@
+package divide_and_conquer_practice;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class TowerOfHanoi {
+	
+	static int count = 1;
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		move(n, 1, 3);
+		
+		System.out.println(count);
+		System.out.println(sb.toString());
+		
+		br.close();
+	}
+	
+	public static void move(int no, int x, int y) { 
+		if(no > 1) {
+			count++;
+			move(no-1, x, 6 - x - y);
+		}
+		
+		sb.append(x + " " + y + "\n");
+		
+		if(no > 1) {
+			count++;
+			move(no-1, 6 - x - y, y);
+		}
+	}
+}
