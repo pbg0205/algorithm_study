0e6d873650f6b804087736499dab795c054847f5
pbg0205 <pbg0205@naver.com>
2020-09-09 00:51:05 +0900

    [mod] 알고리즘 문제 수정

--- Baekjoon/src/codeplus/basic/tree/baekjoon5639.java
+++ Baekjoon/src/codeplus/basic/tree/baekjoon5639.java
@@ -1,9 +1,13 @@
 package codeplus.basic.tree;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Scanner;
+
+/*
+ * @Problem     이진 검색 트리(5639)
+ * @Author      pbg0205
+ * @Created by  09.09.20
+ *
+ */
 
 public class baekjoon5639 {
     public static void main(String[] args) {
