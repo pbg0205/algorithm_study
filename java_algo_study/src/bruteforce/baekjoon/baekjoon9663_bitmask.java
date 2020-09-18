507f9bf72a39343c9664a35c573ad0dcc032aa46
pbg0205 <pbg0205@naver.com>
2020-09-19 00:10:54 +0900

    N-Queen(9663) : bitmask 문제 풀이

---
+++ java_algo_study/src/bruteforce/baekjoon/baekjoon9663_bitmask.java
@@ -0,0 +1,42 @@
+package bruteforce.baekjoon;
+
+import java.io.*;
+
+/*
+ * @Problem     N-Queen(9663) :https://www.acmicpc.net/problem/9663
+ * @Author      pbg0205
+ * @Created by   09.18.20
+ *
+ */
+public class baekjoon9663_bitmask {
+
+    static int N, line, res;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        line = (1<<N) -1;
+        solve(0, 0, 0, 0);
+
+        System.out.println(res);
+
+        System.out.println(Integer.toBinaryString(-3)); //3 : 011
+    }
+
+    private static void solve(int cnt, int horizon, int ld, int rd) {
+        if(cnt == N){
+            res++;
+            return ;
+        }
+
+        int ok = (~(horizon| ld | rd)) & line;
+        while(ok > 0){
+            int i = ok & -ok;
+
+            solve(cnt+1, horizon | i, (ld|i)>>1, (rd|i)<<1);
+
+            ok ^= i;
+        }
+    }
+}
