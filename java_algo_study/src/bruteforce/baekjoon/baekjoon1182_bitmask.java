90167e6d2039f6a97b339c5cf004825f3887ee66
pbg0205 <pbg0205@naver.com>
2020-09-21 19:13:53 +0900

    토마토(7576) : bfs 문제풀이

---
+++ java_algo_study/src/bruteforce/baekjoon/baekjoon1182_bitmask.java
@@ -0,0 +1,44 @@
+package bruteforce.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+ * @Problem     부분 수열의 합 : https://www.acmicpc.net/problem/1182
+ * @Author      pbgu0205
+ * @Created by  09.19.20
+ */
+public class baekjoon1182_bitmask {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        int subSet_cnt = 0;
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int subSet = 1; subSet < (1 << N); subSet++) {//전체 집합에서 부분집합 탐색
+            int sum_tmp = 0;
+            for (int idx = 0; idx < N; idx++) {
+                if (((1 << idx) & subSet) > 0) {
+                    sum_tmp += arr[idx];
+                }
+            }
+
+            if (sum_tmp == S) {
+                subSet_cnt++;
+            }
+        }
+
+        System.out.println(subSet_cnt);
+    }
+}
