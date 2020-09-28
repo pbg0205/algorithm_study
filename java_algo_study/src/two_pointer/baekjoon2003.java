1ceccc263fe741a06bf1d72bbf39ef1eb7479577
pbg0205 <pbg0205@naver.com>
2020-09-28 22:53:49 +0900

    수드르이 합(2003)

---
+++ java_algo_study/src/two_pointer/baekjoon2003.java
@@ -0,0 +1,47 @@
+package two_pointer;
+
+import java.io.*;
+import java.util.*;
+
+/*
+ * @problem    수들의 함2(2003)
+ * @author      pbg0205
+ * @created by  09.28.20
+ */
+public class baekjoon2003 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int L = 0, R = 0;
+        int sum = 0;
+        int result = 0;
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        while(true){
+            if(sum >= M){
+               sum -= arr[L++];
+            }else if(R == N){
+                break;
+            }else{
+                sum += arr[R++];
+            }
+
+            if(sum == M){
+                result++;
+            }
+        }
+
+        System.out.println(result);
+
+        br.close();
+    }
+}
