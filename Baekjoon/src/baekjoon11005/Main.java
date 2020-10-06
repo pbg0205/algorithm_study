ee9d9aedbfe24a6c1917ddce839c8c492a6bfa84
pbg0205 <pbg0205@naver.com>
2020-10-07 01:53:32 +0900

    진법 변환2(11005)

--- Baekjoon/src/baekjoon11005/Main.java
+++ Baekjoon/src/baekjoon11005/Main.java
@@ -1,9 +1,11 @@
-package codeplus.basic.math1_ref;
-
+package baekjoon11005;
+/*
+ * @problem		진법 변환2(11005) : https://www.acmicpc.net/problem/11005
+ * @author		pbg0205
+ */
 import java.util.Scanner;
 
-//문제 : 진법 변환2(11005)
-public class base_transformation2 {
+public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int input = sc.nextInt();
