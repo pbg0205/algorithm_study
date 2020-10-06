f3f9e68f4b6a147f1de08d4d1f56cba1a11aee2a
pbg0205 <pbg0205@naver.com>
2020-10-06 22:18:56 +0900

    수 이어 쓰기1(1748)

--- Baekjoon/src/baekjoon1748/Main.java
+++ Baekjoon/src/baekjoon1748/Main.java
@@ -1,10 +1,13 @@
-package codeplus.basic.brute_force;
-
+package baekjoon1748;
+/*
+ * @problem		수 이어 쓰기1(1748) : https://www.acmicpc.net/problem/1748
+ * @author		pbg0205
+ */
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class overWrite_num {
+public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
