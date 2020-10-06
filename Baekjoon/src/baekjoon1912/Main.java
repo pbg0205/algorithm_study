c344f390c05ebe1a4deaec0d4fac6f9e4105f17a
pbg0205 <pbg0205@naver.com>
2020-10-07 00:19:05 +0900

    연속합(1912)

--- Baekjoon/src/baekjoon1912/Main.java
+++ Baekjoon/src/baekjoon1912/Main.java
@@ -1,11 +1,14 @@
-package codeplus.basic.dp1;
-
+package baekjoon1912;
+/*
+ * @problem		연속합(1912) : https://www.acmicpc.net/problem/1912
+ * @author		pbg0205
+ */
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Realy_sum {
+public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
