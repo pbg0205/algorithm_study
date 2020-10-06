992fb3ce5cf11cdc47917a6a2b823b2f6d1be0f9
pbg0205 <pbg0205@naver.com>
2020-10-06 22:58:37 +0900

    카잉달력(6064)

--- Baekjoon/src/baekjoon6064/Main.java
+++ Baekjoon/src/baekjoon6064/Main.java
@@ -1,11 +1,14 @@
-package codeplus.basic.brute_force;
-
+package baekjoon6064;
+/*
+ * @problem		카잉달력(6064) : https://www.acmicpc.net/problem/6064
+ * @author		pbg0205
+ */
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Kying_calendar {
+public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
