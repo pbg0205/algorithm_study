6043fc83303a2582d0cd8551fcad3fe59cb0fe98
pbg0205 <pbg0205@naver.com>
2020-10-07 00:14:24 +0900

    2xN타일링(11726)

--- Baekjoon/src/baekjoon11726/Main.java
+++ Baekjoon/src/baekjoon11726/Main.java
@@ -1,8 +1,11 @@
-package codeplus.basic.dp1;
-
+package baekjoon11726;
+/*
+ * @problem		2xN타일링(11726) : https://www.acmicpc.net/problem/11726
+ * @author		pbg0205
+ */
 import java.util.Scanner;
 
-public class n_2_tiling {
+public class Main {
 	
 	static int[] d = new int[1001];
 	public static void main(String[] args) {
