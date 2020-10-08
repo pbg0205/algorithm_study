180f496abe1929dc42f9a91d0dee88689473549e
pbg0205 <pbg0205@naver.com>
2020-10-08 20:31:02 +0900

    네 수(10824)

--- Baekjoon/src/baekjoon10824/Main.java
+++ Baekjoon/src/baekjoon10824/Main.java
@@ -4,10 +4,9 @@ package baekjoon10824;
  * @author		pbg0205
  */
 
-/
 import java.util.Scanner;
 
-public class Main {
+class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
