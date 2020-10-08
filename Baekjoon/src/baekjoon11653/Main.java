72a61de9509c94252c99892f63b3e6e7a9a62ef6
pbg0205 <pbg0205@naver.com>
2020-10-08 23:11:46 +0900

    소인수분해(11653)

--- Baekjoon/src/baekjoon11653/Main.java
+++ Baekjoon/src/baekjoon11653/Main.java
@@ -6,7 +6,7 @@ package baekjoon11653;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Main {
+class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
