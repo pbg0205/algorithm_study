93bce7ae98bf820ec2012bd76149b8b17380ece1
pbg0205 <pbg0205@naver.com>
2020-10-08 23:41:17 +0900

    단어뒤집기(9093)

--- Baekjoon/src/baekjoon9093/Main.java
+++ Baekjoon/src/baekjoon9093/Main.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 
-public class Main {
+class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int test_case = Integer.parseInt(br.readLine());
