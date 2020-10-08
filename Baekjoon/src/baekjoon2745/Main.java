0e11acc19b03dbf96e35eb4bd192bf94597456a3
pbg0205 <pbg0205@naver.com>
2020-10-08 23:39:19 +0900

    진법 변환(2745)

--- Baekjoon/src/baekjoon2745/Main.java
+++ Baekjoon/src/baekjoon2745/Main.java
@@ -5,7 +5,7 @@ package baekjoon2745;
  */
 import java.util.Scanner;
 
-public class Main {
+class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String input = sc.next();
