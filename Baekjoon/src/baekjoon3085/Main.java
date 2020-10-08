6d64f59670a691b1857fdd9566eb642bdb8bb856
pbg0205 <pbg0205@naver.com>
2020-10-08 23:39:29 +0900

    사탕게임(3085)

--- Baekjoon/src/baekjoon3085/Main.java
+++ Baekjoon/src/baekjoon3085/Main.java
@@ -7,7 +7,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+class Main {
 	static char[][] data;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
