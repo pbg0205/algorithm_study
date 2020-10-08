8b02543e6140526d6ec0d7192cc30dfbea2d7c9a
pbg0205 <pbg0205@naver.com>
2020-10-08 23:26:21 +0900

    오등큰수(17299)

--- Baekjoon/src/baekjoon17299/Main.java
+++ Baekjoon/src/baekjoon17299/Main.java
@@ -8,7 +8,7 @@ import java.io.*;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
-public class Main {
+class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
