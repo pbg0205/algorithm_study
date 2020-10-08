153d4ed89b9c57fca1316552aacac8a638ca5c03
pbg0205 <pbg0205@naver.com>
2020-10-08 23:22:01 +0900

    1만들기(1463) - TopDown방식

--- Baekjoon/src/baekjoon1463/Main_TopDown.java
+++ Baekjoon/src/baekjoon1463/Main_TopDown.java
@@ -6,7 +6,7 @@ package baekjoon1463;
 
 import java.util.Scanner;
 
-public class Main_TopDown {
+class Main_TopDown {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
