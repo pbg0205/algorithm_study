ea125c3fc88cd6ff43c0abb83483ee0446896188
pbg0205 <pbg0205@naver.com>
2020-10-08 23:08:03 +0900

    팩토리얼(10872)

--- Baekjoon/src/baekjoon10872/Main.java
+++ Baekjoon/src/baekjoon10872/Main.java
@@ -5,7 +5,7 @@ package baekjoon10872;
  */
 import java.util.Scanner;
 
-public class Main {
+class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		long n = Long.parseLong(sc.nextLine());
