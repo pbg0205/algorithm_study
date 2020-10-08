7084cc1bb4b23feccf3711e9bce1c8f212b181d8
pbg0205 <pbg0205@naver.com>
2020-10-08 23:20:37 +0900

    에디터(1406)

--- Baekjoon/src/baekjoon1406/Main.java
+++ Baekjoon/src/baekjoon1406/Main.java
@@ -8,7 +8,7 @@ package baekjoon1406;
 import java.io.*;
 import java.util.*;
 
-public class Main {
+class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
