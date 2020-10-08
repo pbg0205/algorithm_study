71605020405f303f4e8aae07bdd354bc0d93fd5f
pbg0205 <pbg0205@naver.com>
2020-10-08 23:32:06 +0900

    단어뒤집기2(17413)

--- Baekjoon/src/baekjoon17413/Main.java
+++ Baekjoon/src/baekjoon17413/Main.java
@@ -10,7 +10,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
 
-public class Main {
+class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
