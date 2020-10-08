c03d3b9f9850c6dc8128b16aea99269fc22b6ce8
pbg0205 <pbg0205@naver.com>
2020-10-08 23:39:42 +0900

    균형잡힌 세상 (4949)

--- Baekjoon/src/baekjoon4949/Main.java
+++ Baekjoon/src/baekjoon4949/Main.java
@@ -12,7 +12,7 @@ import java.util.Stack;
  * author		{pbg0205}
  * Create by	{08.24.20}
 */
-public class Main {
+class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
