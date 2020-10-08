895fc127d2ab2926638b688a85a0b0c2364ab5d9
pbg0205 <pbg0205@naver.com>
2020-10-08 23:08:44 +0900

    이전 순열(10973)

--- Baekjoon/src/baekjoon10973/Main.java
+++ Baekjoon/src/baekjoon10973/Main.java
@@ -6,7 +6,7 @@ package baekjoon10973;
 import java.io.*;
 import java.util.StringTokenizer;
 
-public class Main {
+class Main {
 	static int[] data;
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
