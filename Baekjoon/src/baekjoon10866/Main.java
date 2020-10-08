96d1f054afa87ab2f99662b765ae99c6c456d65c
pbg0205 <pbg0205@naver.com>
2020-10-08 23:07:53 +0900

    덱(10866)

--- Baekjoon/src/baekjoon10866/Main.java
+++ Baekjoon/src/baekjoon10866/Main.java
@@ -8,7 +8,7 @@ import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Scanner;
 
-public class Main {
+class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		Deque<Integer> deque = new LinkedList<>();
