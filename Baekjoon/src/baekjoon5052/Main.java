a529843336803874547ac1d555f0d09daa0240bd
pbg0205 <pbg0205@naver.com>
2020-10-20 22:04:46 +0900

    전화번호 목록(5052) - 메서드 분리

--- Baekjoon/src/baekjoon5052/Main.java
+++ Baekjoon/src/baekjoon5052/Main.java
@@ -3,7 +3,7 @@ package baekjoon5052;
  * @problem     전화번호 목록(5052)
  * @url         https://www.acmicpc.net/problem/5052
  * @author      pbg0205
- * @created by  10.13.20
+ * @created by  10.20.20
  */
 
 import java.io.*;
@@ -11,31 +11,68 @@ import java.util.*;
 
 class Main {
 
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static StringBuilder sb  = new StringBuilder();
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int testCase = Integer.parseInt(br.readLine());
 
-        while(testCase-- > 0){
-            int size = Integer.parseInt(br.readLine());
-            String[] phoneNumber = new String[size];
-            String result = "YES";
+        checkResult(testCase);
 
-            for (int index = 0; index < size; index++) {
-                phoneNumber[index] = br.readLine();
+        printResult();
+    }
+
+    private static void checkResult(int size) throws IOException {
+        int numberOfTelephone;
+        ArrayList<String> telephoneList;
+
+        while(size-- > 0){
+            numberOfTelephone = addIntByConsole();
+            telephoneList = addListByConsole(numberOfTelephone);
+
+            if(isConsistent(telephoneList)){
+                sb.append("YES \n");
+            }else{
+                sb.append("NO \n");
             }
+        }
+    }
 
-            Arrays.sort(phoneNumber);
+    private static boolean isConsistent(ArrayList<String> telephoneList) throws IOException {
+        return checkConsitency(telephoneList);
+    }
 
-            for (int index = 0; index < size - 1; index++) {
-                if(phoneNumber[index+1].startsWith(phoneNumber[index])){
-                    result = "NO";
-                    break;
-                }
+    private static boolean checkConsitency(ArrayList<String> telephoneList) {
+        int listSize = telephoneList.size();
+        Collections.sort(telephoneList);
+
+        for (int i = 0; i < listSize - 1; i++) {
+            String nowNumber = telephoneList.get(i);
+            String nextNumber = telephoneList.get(i + 1);
+
+            if(nextNumber.startsWith(nowNumber)){
+                return false;
             }
-
-            System.out.println(result);
         }
 
-        br.close();
+        return true;
+    }
+
+    private static int addIntByConsole(int value) throws IOException{
+        return Integer.parseInt(br.readLine());
+    }
+
+    private static ArrayList<String> addListByConsole(int numberOfTelephone) throws IOException {
+        ArrayList<String> list = new ArrayList<>();
+
+        for (int i = 0; i < numberOfTelephone; i++) {
+            list.add(br.readLine());
+        }
+
+        return list;
+    }
+
+    private static void printResult() {
+        System.out.println(sb);
     }
 }
