1cbc670f265cb281cdb70a0f6eb2d833035a9af0
pbg0205 <pbg0205@naver.com>
2020-10-23 21:45:50 +0900

    가장 긴 증가하는 부분 수열(11053)

--- Baekjoon/src/baekjoon11053/Main.java
+++ Baekjoon/src/baekjoon11053/Main.java
@@ -1,37 +1,49 @@
 package baekjoon11053;
-/*
- * @problem		가장 긴 증가하는 부분 수열(11053) : https://www.acmicpc.net/problem/11053
- * @author		pbg0205
- */
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
+/*
+ * @Problem     가장 긴 증가하는 부분 수열(11053)
+ * @Author      pbg0205
+ * @Created by  09.09.20
+ */
+
 class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		int n = Integer.parseInt(br.readLine());
-		int[] num = new int[n];
-		int[] dp = new int[n];
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
+        int max = Integer.MIN_VALUE;
 
-		for (int i = 0; i < n; i++)	num[i] = Integer.parseInt(st.nextToken());	
-		Arrays.fill(dp, 1);
-		
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < i; j++) {
-				if(num[i] > num[j] && dp[i] <= dp[j])	dp[i] = dp[j] + 1;
-			}
-		}
-		
-		int max = 0;
-		for (int i = 0; i < n; i++)	
-			if(max < dp[i])	max = dp[i];
-		
-		System.out.println(max);
-	}
+        int data[] = new int[n];
+        int dp[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            data[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.fill(dp, 1);
+
+        for (int now = 1; now < n; now++) {
+            for (int prev = 0; prev < now; prev++) {
+                if( dp[prev] >= dp[now] && data[prev] < data[now]){
+                    dp[now] = dp[prev] + 1;
+                }
+            }
+        }
+
+        for (int j = 0; j < n; j++) {
+            max = Math.max(max, dp[j]);
+        }
+
+
+
+
+        System.out.println(max);
+    }
 }
