509716b8be1f7a60b2ee1e942e99ab5b1b1a8270
pbg0205 <pbg0205@naver.com>
2020-10-23 20:14:29 +0900

    중량제한(1939)

--- Baekjoon/src/baekjoon1939/Main.java
+++ Baekjoon/src/baekjoon1939/Main.java
@@ -1,4 +1,4 @@
-package codeplus.middle1.binarySearch;
+package baekjoon1939;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -15,17 +15,17 @@ import java.util.StringTokenizer;
  * 
  */
 
-public class baejoon1939 {
-	
+class Main {
+
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
+
 		int start, end;
-		
+
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
-		
+
 		ArrayList<BridgeInfo>[] bridges = new ArrayList[n+1];
 		for (int i = 1; i <= n; i++) {
 			bridges[i] = new ArrayList<>();
@@ -37,16 +37,16 @@ public class baejoon1939 {
 			int s = Integer.parseInt(st.nextToken());
 			int e = Integer.parseInt(st.nextToken());
 			int c = Integer.parseInt(st.nextToken());
-			
+
 			bridges[s].add(new BridgeInfo(e, c));
 			bridges[e].add(new BridgeInfo(s, c));
 			max = Math.max(max, c);
 		}
-		
+
 		st = new StringTokenizer(br.readLine());
 		start = Integer.parseInt(st.nextToken());
 		end = Integer.parseInt(st.nextToken());
-		
+
 		int left = 1;
 		int right = max;
 		int ans = 0;
@@ -60,20 +60,20 @@ public class baejoon1939 {
 				right = mid-1;
 			}
 		}
-		
+
 		System.out.println(ans);
-		
+
 	}
 
 	private static boolean go(ArrayList<BridgeInfo>[] bridges, int mid, int start, int end, boolean[] visited) {
 		if(visited[start])
 			return false;
-		
+
 		visited[start] = true;
-		
+
 		if(start == end)
 			return true;
-		
+
 		for (BridgeInfo bi : bridges[start]) {
 			if(bi.cost >= mid) {
 				if(go(bridges, mid, bi.next, end, visited)) {
