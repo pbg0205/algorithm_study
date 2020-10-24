dbfe52e66bd07b63ea62f57d830929c1462de178
pbg0205 <pbg0205@naver.com>
2020-10-25 01:48:50 +0900

    광고(1305)

--- Baekjoon/src/baekjoon1305/Main.java
+++ Baekjoon/src/baekjoon1305/Main.java
@@ -1,8 +1,6 @@
-package codeplus.middle2.stringAlgorithm1;
+package baekjoon1305;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 /*
  * @Problem     광고(1305)
@@ -10,7 +8,7 @@ import java.io.InputStreamReader;
  * @Created by  09.02.20
  *
  */
-public class baekjoon1305 {
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
