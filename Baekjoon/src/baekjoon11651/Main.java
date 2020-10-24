640304d515b8b3116dae0ca76e15e543b743980c
pbg0205 <pbg0205@naver.com>
2020-10-25 01:53:09 +0900

    좌표 정렬하기2(11651)

--- Baekjoon/src/baekjoon11651/Main.java
+++ Baekjoon/src/baekjoon11651/Main.java
@@ -1,19 +1,17 @@
-package sorting;
+package baekjoon11651;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 /*
  * @Problem      좌표 정렬하기2(11651)
+ * @url         https://www.acmicpc.net/problem/11652
  * @Author       pbg0205
  * @Created by   09.02.20
  *
  */
 
-public class baekjoon11651 {
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
