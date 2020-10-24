ec6c9ddb985d95a03469ec8bb0845f90027bb03e
pbg0205 <pbg0205@naver.com>
2020-10-25 01:31:11 +0900

    카드2(2164)

--- Baekjoon/src/baekjoon2164/Main.java
+++ Baekjoon/src/baekjoon2164/Main.java
@@ -1,10 +1,14 @@
-package queue;
+package baekjoon2164;
 
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.*;
 
-public class card2 {
+/*
+ * @Problem     카드2(2164)
+ * @url			https://www.acmicpc.net/problem/2164
+ * @Author      pbg0205
+ */
+
+class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = Integer.parseInt(sc.nextLine());
