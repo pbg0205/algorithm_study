871174b5bff7b0cd0999327020948caa45532640
pbg0205 <pbg0205@naver.com>
2020-10-25 01:46:49 +0900

    거스름돈(14916)

--- Baekjoon/src/baekjoon14916/Main.java
+++ Baekjoon/src/baekjoon14916/Main.java
@@ -1,17 +1,14 @@
-package dp;
+package baekjoon14916;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 /*
  * @Problem     거스름돈(14916)
  * @Author      pbg0205
  * @Created by  09.08.20
- *
  */
 
-public class bakjoon14916 {
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
