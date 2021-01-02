47cc4a136d36967e369838e258479e90c62bd77b
pbg0205 <pbg0205@naver.com>
2021-01-02 18:34:45 +0900

    Z(1074)

--- Baekjoon/src/baekjoon1074/Main.java
+++ Baekjoon/src/baekjoon1074/Main.java
@@ -11,48 +11,53 @@ import java.util.*;
 
 
 class Main {
-	
-	static int[][] map;
-	static int r, c;
-	
-	static int[] dy = {0, 0, 1, 1};
-	static int[] dx = {0, 1, 0, 1};
-	
-	
-	public static void main(String[] args) throws IOException{
+	static int row, col;
+
+	static int[] dx = {0, 0, 1, 1};
+	static int[] dy = {0, 1, 0, 1};
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
+
 		int n = Integer.parseInt(st.nextToken());
-		
-		int size = (int)Math.pow(2, n);
-		r = Integer.parseInt(st.nextToken());
-		c = Integer.parseInt(st.nextToken());
-		
+		int size = (int) Math.pow(2, n);
+
+		row = Integer.parseInt(st.nextToken());
+		col = Integer.parseInt(st.nextToken());
+
 		divide(0, 0, 0, size);
-		
+
+		br.close();
 	}
-	
-	private static void divide(int y, int x, int cnt, int size) {
-		if( y + size <= r || y > r
-				|| x + size <= c || x > c )	return ;
-		
-		if(size == 2) {
-			for (int i = 0; i < 4; i++) {
-				int ny = dy[i] + y;
-				int nx = dx[i] + x;
-				
-				if(ny == r && nx == c)
-					System.out.println(cnt + i);
-			}
-			return ;
+
+	private static void divide(int x, int y, int cnt, int size) {
+		System.out.println(x + " " + y + " " + size);
+		if (!isBoundary(x, y, size)) {
+			return;
 		}
-		
+
+		if (size == 2) {
+			for (int i = 0; i < 4; i++) {
+				int nx = dx[i] + x;
+				int ny = dy[i] + y;
+
+				if (nx == row && ny == col) {
+					System.out.println(cnt + i);
+				}
+			}
+			return;
+		}
+
 		int newSize = size / 2;
-		
-		divide(y, x, cnt, newSize);
-		divide(y, x + newSize, cnt + (newSize * newSize), newSize);
-		divide(y + newSize, x, cnt + (newSize * newSize * 2), newSize);
-		divide(y + newSize, x + newSize, cnt + (newSize * newSize * 3), newSize);
+
+		divide(x, y, cnt, newSize);
+		divide(x, y + newSize, cnt + (newSize * newSize), newSize);
+		divide(x + newSize, y, cnt + (newSize * newSize * 2), newSize);
+		divide(x + newSize, y + newSize, cnt + (newSize * newSize * 3), newSize);
+	}
+
+	private static boolean isBoundary(int x, int y, int size) {
+		return (x > row - size && x <= row) && (y > col - size && y <= col);
 	}
 }
