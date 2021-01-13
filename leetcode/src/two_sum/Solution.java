49c4878b529020a8be034cfafae9f616e1e6ecb4
pbg0205 <pbg0205@naver.com>
2021-01-13 11:08:53 +0900

    two_sum

---
+++ leetcode/src/two_sum/Solution.java
@@ -0,0 +1,44 @@
+package two_sum;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int len = nums.length;
+        int[] answer;
+
+        for (int left_idx = 0; left_idx < len; left_idx++) {
+            answer = iterateRightIndex(nums, target, left_idx);
+
+            if(answer != null) {
+                return answer;
+            }
+        }
+        return null;
+    }
+
+    private int[] iterateRightIndex(int[] nums, int target, int left_idx) {
+        int[] answer = new int[2];
+
+        for (int right_idx = left_idx + 1; right_idx < nums.length; right_idx++) {
+            int sum = nums[left_idx] + nums[right_idx];
+
+            if(sum == target) {
+                answer[0] = left_idx;
+                answer[1] = right_idx;
+                return answer;
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 4};
+        int target = 6;
+
+        Solution solution = new Solution();
+        int[] answer = solution.twoSum(nums, target);
+
+        for (int i : answer) {
+            System.out.println(i);
+        }
+    }
+}
