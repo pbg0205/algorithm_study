27d27a3f74420e0be42dd988b9e33073091a352f
pbg0205 <pbg0205@naver.com>
2021-02-03 19:56:27 +0900

    [프로그래머스] - 보석 쇼핑 리팩토링

--- programmers/src/kakao/jewerly_shopping/Solution.java
+++ programmers/src/kakao/jewerly_shopping/Solution.java
@@ -1,13 +1,13 @@
 package kakao.jewerly_shopping;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Solution {
-    private static final Set<String> set = new HashSet<>();
-    private static final Map<String, Integer> map = new HashMap<>();
+    private final Set<String> set = new HashSet<>();
+    private final Map<String, Integer> map = new HashMap<>();
+    private static int MIN_LENGTH;
+    private static int MIN_START_INDEX;
+
 
     public int[] solution(String[] gems) {
         int[] answer = new int[2];
@@ -22,8 +22,8 @@ public class Solution {
         int end = 0;
 
         int nowLength;
-        int minLength = gems.length;
-        int minStartIndex = gems.length;
+        MIN_LENGTH = gems.length;
+        MIN_START_INDEX = gems.length;
 
         if (set.size() == 1) {
             return new int[]{1, 1};
@@ -31,39 +31,55 @@ public class Solution {
 
         while (start < gems.length) {
             nowLength = end - start + 1;
-            if (nowLength > minLength || end == gems.length) {
-                if (map.get(gems[start]) == 1) {//막혔던 부분
-                    map.remove(gems[start]);
-                } else {
-                    map.put(gems[start], map.get(gems[start]) - 1);
-                }
+            if (nowLength > MIN_LENGTH || end == gems.length) {
+                removeElement(gems[start]);
                 start++;
             } else {
-                if (!map.containsKey(gems[end])) {
-                    map.put(gems[end], 1);
-                } else {
-                    map.put(gems[end], map.get(gems[end]) + 1);
-                }
+                addElement(gems[end]);
                 end++;
             }
 
             if (map.size() == set.size()) {
-                if(end - start < minLength) {
-                    minLength = end - start; // 막혔던 부분
-                    minStartIndex = start;
-                }
-                if(nowLength == minLength) {
-                    if(minStartIndex > start) {
-                        minStartIndex = start;
-                    }
-                }
+                getMinLength(start, end);
+                getMinIndex(start, nowLength);
             }
         }
 
-        answer[0] = minStartIndex + 1;
-        answer[1] = answer[0] + minLength - 1;
+        answer[0] = MIN_START_INDEX + 1;
+        answer[1] = answer[0] + MIN_LENGTH - 1;
 
         return answer;
+    }
+
+    private void getMinLength(int start, int end) {
+        if (end - start < MIN_LENGTH) {
+            MIN_LENGTH = end - start; // 막혔던 부분
+            MIN_START_INDEX = start;
+        }
+    }
+
+    private void getMinIndex(int start, int nowLength) {
+        if (nowLength == MIN_LENGTH) {
+            if (MIN_START_INDEX > start) {
+                MIN_START_INDEX = start;
+            }
+        }
+    }
+
+    void removeElement(String element) {
+        if (map.get(element) == 1) {//막혔던 부분
+            map.remove(element);
+        } else {
+            map.put(element, map.get(element) - 1);
+        }
+    }
+
+    void addElement(String element) {
+        if (!map.containsKey(element)) {
+            map.put(element, 1);
+        } else {
+            map.put(element, map.get(element) + 1);
+        }
     }
 
     public static void main(String[] args) {
