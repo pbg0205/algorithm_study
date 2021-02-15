500314dafe45ec1cb79109ac867a8d83142a01d8
pbg0205 <pbg0205@naver.com>
2021-02-15 22:41:27 +0900

    분산 처리(1009)

---
+++ Baekjoon/src/baekjoon1009/Main.java
@@ -0,0 +1,24 @@
+package Baekjoon.src.baekjoon1009;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+        int a, b, r;
+
+        for (int i = 0; i < t; i++) {
+            a = s.nextInt();
+            b = s.nextInt();
+            r = 1;
+            for (int j = 0; j < b; j++) {
+                r = a * r % 10;
+            }
+
+            if (r == 0) r = 10;
+
+            System.out.println(r);
+        }
+    }
+}
