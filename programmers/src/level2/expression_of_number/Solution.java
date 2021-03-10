4ee919222ea66380483e58b0205f95755f9c5211
pbg0205 <pbg0205@naver.com>
2021-03-10 21:41:29 +0900

    [프로그래머스] 수의 표현

--- programmers/src/level2/expression_of_number/Solution.java
+++ programmers/src/level2/expression_of_number/Solution.java
@@ -23,12 +23,23 @@ public class Solution {
         return answer;
     }
 
+    public int solution2(int num) {
+        int answer = 0;
+        for (int i = 1; i <= num; i += 2) {
+            if (num % i == 0) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+
     public static void main(String[] args) {
         Solution solution = new Solution();
         System.out.println(solution.solution(15));
     }
 }
 
+// [2. 풀이법 설명]
 // 1) x를 홀수개의 연속된 자연수의 합으로 표현할 수 있는 경우
 // - 1개 : b => 1*b [단, b >= 1]
 // - 3개 : b-1, b, b+1 => 3*b [단, b >= 2]
