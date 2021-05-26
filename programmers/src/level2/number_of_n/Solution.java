361c6cabc668d3f786f2b840f4a66825568226a6
pbg0205 <pbg0205@naver.com>
2021-05-27 08:58:36 +0900

    [프로그래머스] n진수(오류 케이스 수정)

--- programmers/src/level2/number_of_n/Solution.java
+++ programmers/src/level2/number_of_n/Solution.java
@@ -2,36 +2,34 @@ package programmers.src.level2.number_of_n;
 
 public class Solution {
     public String solution(int n, int t, int m, int p) {
-        String totalNumbersStr = "0";
+        String totalNumberStr = "0";
         int count = 0;
 
-        while (totalNumbersStr.length() < (t * m + p)) {
+        while (totalNumberStr.length() < (t * m + p)) {
             String byNum = "";
             int num = count++;
 
-            while(num != 0) {
+            while (num != 0) {
                 int remainder = num % n;
 
-                if(remainder >= 10) {
+                if (remainder >= 10) {
                     remainder -= 10;
-                    byNum = String.valueOf((char) (remainder + 'A'));
+                    byNum += String.valueOf((char) (remainder + 'A'));
                 } else {
-                    byNum = String.valueOf((char) remainder);
+                    byNum += String.valueOf(remainder);
                 }
 
                 num /= n;
 
             }
 
-            totalNumbersStr += new StringBuffer(byNum).reverse().toString();
-
+            totalNumberStr += new StringBuffer(byNum).reverse().toString();
         }
 
         String answer = "";
 
-        for (int i = 0; i < t; i++) {
-            answer += String.valueOf(totalNumbersStr.charAt(m * i + (p - 1)));
-        }
+        for (int i = 0; i < t; i++)
+            answer += String.valueOf(totalNumberStr.charAt(m * i + p - 1));
 
         return answer;
     }
