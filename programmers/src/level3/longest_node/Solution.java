4e613a28772fe2d6bc9f43ff784812447081a4f8
pbg0205 <pbg0205@naver.com>
2021-06-06 23:03:43 +0900

    [프로그래머스] 가장 먼 노드

--- programmers/src/level3/longest_node/Solution.java
+++ programmers/src/level3/longest_node/Solution.java
@@ -1,34 +1,60 @@
 package programmers.src.level3.longest_node;
 
+
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class Solution {
-    public int bfs(int n, int[][] edge){
-        boolean[] check = new boolean[n];
-        boolean[][] connect = new boolean[n][n];
-        Queue<Integer> q = new LinkedList<>();
-        for(int i=0; i<edge.length; i++){
-            connect[edge[i][0]-1][edge[i][1]-1]=true;
-            connect[edge[i][1]-1][edge[i][0]-1]=true;
-        }
-        check[0]=true;
-        q.add(0);
-
+    public int solution(int n, int[][] edge) {
         int answer = 0;
-        while(!q.isEmpty()){
-            int qSize = q.size();
-            for(int i=0; i<qSize;i++){
-                int node = q.poll();
-                for(int j=0; j<n; j++){
-                    if(connect[j][node]&&!check[j]){
-                        check[j]=true;
-                        q.add(j);
-                    }
+
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+
+        for (int i = 0; i < edge.length; i++) {
+            list.add(new ArrayList<>());
+        }
+
+        int a, b;
+        for (int[] node : edge) {
+            a = node[0];
+            b = node[1];
+
+            list.get(a).add(b);
+            list.get(b).add(a);
+        }
+
+        int[] count = new int [n + 1];
+        boolean[] visited = new boolean[n + 1];
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.add(1);
+        visited[0] = visited[1] = true;
+
+        int now;
+
+        while (!queue.isEmpty()) {
+            now = queue.poll();
+
+            for (int v : list.get(now)) {
+                if(!visited[v]) {
+                    count[v] = count[now] + 1;
+                    visited[v] = true;
+                    queue.add(v);
                 }
             }
-            answer=qSize;
         }
+
+        int max = 0;
+        for (int cnt : count) {
+            if(max < cnt) {
+                max = cnt;
+                answer = 1;
+            } else if(max == cnt) {
+                answer++;
+            }
+        }
+
         return answer;
     }
 }
